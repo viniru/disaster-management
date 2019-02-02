2f3e55c4523f54e3da187976302697c608fc0712
vinayak bhat <viniru100@gmail.com>
2019-02-02 22:00:55 +0530

    register victim (storing the victim info in the ledger complete). check for victim copy already being present implemented along with composite key (reliefcamp-email)

--- chaincode/src/github.com/trade_workflow_v1/accessControlUtils.go
+++ chaincode/src/github.com/trade_workflow_v1/accessControlUtils.go
@@ -1,19 +1,3 @@
-/*
- * Copyright 2018 IBM All Rights Reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the 'License');
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an 'AS IS' BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package main
 
 import (
@@ -22,7 +6,6 @@ import (
 	"github.com/hyperledger/fabric/core/chaincode/lib/cid"
 	"crypto/x509"
 )
-
 
 func getTxCreatorInfo(stub shim.ChaincodeStubInterface) (string, string, error) {
 	var mspid string
@@ -47,22 +30,22 @@ func getTxCreatorInfo(stub shim.ChaincodeStubInterface) (string, string, error) 
 // For now, just hardcode an ACL
 // We will support attribute checks in an upgrade
 
-func authenticateExportingEntityOrg(mspID string, certCN string) bool {
-	return (mspID == "ExportingEntityOrgMSP") && (certCN == "ca.exportingentityorg.trade.com")
+func authenticateReliefCamp(mspID string, certCN string) bool {
+	return (mspID == "ReliefCampMSP") && (certCN == "ca.reliefcamp.disaster.com")
 }
 
-func authenticateExporterOrg(mspID string, certCN string) bool {
-	return (mspID == "ExporterOrgMSP") && (certCN == "ca.exporterorg.trade.com")
+func authenticateLocalHub(mspID string, certCN string) bool {
+	return (mspID == "LocalHubMSP") && (certCN == "ca.localhub.disaster.com")
 }
 
-func authenticateImporterOrg(mspID string, certCN string) bool {
-	return (mspID == "ImporterOrgMSP") && (certCN == "ca.importerorg.trade.com")
+func authenticateLogistics(mspID string, certCN string) bool {
+	return (mspID == "LogisticsMSP") && (certCN == "ca.logistics.disaster.com")
 }
 
-func authenticateCarrierOrg(mspID string, certCN string) bool {
-	return (mspID == "CarrierOrgMSP") && (certCN == "ca.carrierorg.trade.com")
+func authenticateNGO(mspID string, certCN string) bool {
+	return (mspID == "NGOMSP") && (certCN == "ca.ngo.disaster.com")
 }
 
-func authenticateRegulatorOrg(mspID string, certCN string) bool {
-	return (mspID == "RegulatorOrgMSP") && (certCN == "ca.regulatororg.trade.com")
+func authenticateGovernment(mspID string, certCN string) bool {
+	return (mspID == "GovernmentMSP") && (certCN == "ca.government.disaster.com")
 }
